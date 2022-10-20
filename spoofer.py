1f31651a4c75b4ce2e7858cccc3003d0f53415cb
Major2000 <majormadobe@gmail.com>
2022-10-20 11:18:39 +0300

    func: update spoofer.py enable ip route

--- spoofer.py
+++ spoofer.py
@@ -16,7 +16,19 @@ def _enable_linux_iproute():
     with open(file_path, "W") as f:
         print(1, file=f)
 
+
+## for windows users 
 def _enable_windows_iproute():
     from services import WService
     service = WService("RemoteAccess")
     service.start()
+
+
+## function below enables IP routing in all platforms
+def enable_ip_route(verbose=True):
+    if verbose:
+        print("[!] Enabling IP Routing...")
+    
+    _enable_windows_iproute() if "nt" in os.name else _enable_linux_iproute()
+    if verbose:
+        print("[!] IP Routing enabled.")
